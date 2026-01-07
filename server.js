6b9ea6b6ba766a7eaf68d85dadb4b812c39b4ebd
priyanshuverma2003 <priyanshuverma952@gmail.com>
2026-01-07 23:11:21 +0530

    Add Vercel configuration

--- server.js
+++ server.js
@@ -79,6 +79,10 @@ app.get('/:page', (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
